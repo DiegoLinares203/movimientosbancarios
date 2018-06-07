664b762893a2f0cf808fd79b6c935bf5e8881dcb
DiegoLinares203 <38674663+DiegoLinares203@users.noreply.github.com>
2018-06-06 19:59:41 -0500

    Proyecto

---
+++ src/main/java/com/diego/app/MovimientosbancariosApplication.java
@@ -0,0 +1,12 @@
+package com.diego.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovimientosbancariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovimientosbancariosApplication.class, args);
+	}
+}
